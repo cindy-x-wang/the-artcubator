a2d6f7b8750f53587dbec6c0b6e1160556d1bc94
leonlu177 <69948642+leonlu177@users.noreply.github.com>
2020-08-19 20:24:54 -0700

    changed allArtists to include our eight partners
    added info for artists, removed ray lol

--- client/src/components/pages/art/allArt.js
+++ client/src/components/pages/art/allArt.js
@@ -13,11 +13,41 @@ export const allArtists = [
         "artistDescription": "Connie Gong (龚智璇) is a seventeen year old artist born in Atlanta, Georgia. She is currently based in Belmont, California, where she attends Carlmont High School. She has studied art under Yunhua Fang for the past ten years. \n \n Connie specializes in charcoal and printmaking, but has recently developed an interest in acrylic painting and graphic design as well. The deeply introspective nature of her art allows her to process the world around her. Creating work is therefore a means by which she can reach new understandings about herself and her environment as they relate to psychology, cultural identity, and emotion. \n \n Her work has been recognized by the Scholastic Art and Writing Awards, as well as her local and state PTA. Additionally, she has been published in The Daphne Review (2019), Navigating the Maze (2019), and the Stanford Anthology for Youth (2017). More of her work can be found on her website.",
     },
     {
-      "fName": "Raymond",
-      "lName": "Banke",
-      "link": "raymondisdumb",
-      "artistDescription": "hi i'm ray and i do dumb things",
+      "fName": "Angela",
+      "lName": "Wei",
+      "link": "angelawei",
+      "artistDescription": "Angela is a multidisciplinary artist majoring in Art History and Visual Arts at Barnard. Throughout her life, she has lived in multiple cities and studied at vastly different colleges, allowing her to develop a global and unique perspective while approaching her work. Her love for art and creativity began early; as a child, she entered into multiple painting competitions and won second place in North America at the United Nations Children Painting Competition and a Top Finalist in Canadian Portrait Society of Canada. In college, she really began to commit to her art more seriously and decided to move to New York to pursue her creative endeavors. Much of her art work’s style consists of highly illustrative narratives, painted with lively bold colors. Angela enjoys experimenting with incorporating different art styles, from traditional Chinese scroll paintings to Renaissance Portraiture. Because of the color palette, her work often alludes to a very bright and happy atmosphere, but upon closer inspection, offers more gruesome elements within. Feel free to connect with Angela over social media or check out her work at the links below. Insta: https://www.instagram.com/theangelawei/ Linkedin:https://www.linkedin.com/in/angela-wei-8180a7164/ Website: https://angelawei.cargo.site/",
     },
+    {
+      "fName": "Cynthia",
+      "lName": "Zhou",
+      "link": "cynthiazhou",
+      "artistDescription": "",
+    },
+    {
+      "fName": "Katrina",
+      "lName": "Liou",
+      "link": "katrinaliou",
+      "artistDescription": "Katrina Liou is an artist and a student at Stanford University studying Product Design and Art Practice. Katrina’s work explores themes of imperfection, human anatomy and the human condition, often asking the question “What does it mean to be human?” Much of her work revolves around depicting the human body to explore the deeply hidden subtleties of the individual. In metaphoric imagery of human interaction, she searches for hints of character, personality, and hidden secrets to embed in the portrayal of flesh, folds of skin, wrinkles, and fat. Through this technique, she depicts the external elements as a display of internal emotion and personality. Examining the human condition through an anatomical lens allows her to engage in an interdisciplinary field that combines two seemingly disparate topics: art and science. At Stanford Katrina currently designs for Mint Magazine (Stanford’s culture and fashion magazine) and creates graphics for the Stanford Daily newspaper. Outside of school she also designs graphics of Hearts Over Hands, a volunteer organization that provides COVID-19 relief for families with young children. More of her work can be found on Instagram @knips00 or online at https://katrinaliou.squarespace.com.",
+    },
+    {
+      "fName": "Liza",
+      "lName": "Evseeva",
+      "link": "lizaevseeva",
+      "artistDescription": "Liza Evseeva is a 2nd year Columbia University student. She hails from the suburbs of DC and originally, from Moscow, Russia. She's a staff illustrator at the Columbia Daily Spectator and a freelance artist and photographer. She believes in constantly pushing one’s comfort zone and experimenting. After 10 years of formal art courses, Liza has a wide range of skills in various traditional and digital mediums and techniques. She can execute still lives, landscapes, portraits, or some combination of those. Her preferred mediums are digital painting and acrylic and oil paint. Liza enjoys working with bold colors, rough textures, and weaving characters into their environments. In her personal artwork, she likes capturing emotion through color and lighting and conveying concepts through surrealist artwork.You can find more info on Liza’s website at newelizzia.wixsite.com/website, or follow her on Instagram @liza.e.art.",
+    },
+    {
+      "fName": "Tao",
+      "lName": "Liu",
+      "link": "taoliu",
+      "artistDescription": "Tao Liu is a 20-year-old Chinese born artist based in New York City and Kunming, China. Inspired by personal life experiences, Liu expresses his understanding of self and the world around him through a series of visual metaphors. His work also deals with the cultural clash between the East and the west. The medium of his work varies from charcoal drawing and oil painting to installation and performance. He has received a number of awards from the Scholastic Arts and Writing Awards, including 8 Gold Keys, 4 Silver Keys, 4 National Gold Metals and American Vision Award. His work The “Praying” Hands was selected into the Art.Write.Now. traveling exhibition in galleries across 4 cities. He was also an American YoungArts Award winner in 2019. He currently studies integrated design at  Parsons School of Design at The New School in New York City. For more information about Liu, please visit his Instagram: the_world_tao_sees, or check out his website: theworldtaosees.com",
+    },
+    {
+      "fName": "Rick",
+      "lName": "Zhang",
+      "link": "rickzhang",
+      "artistDescription": "",
+    }
   ]
   
 export const allArt = [
