a3c79a339ad35a9e77d1b7be576ea88e19bdef8e
Cindy Wang <cindywang2001@gmail.com>
2020-09-01 15:48:30 -0700

    integrated sendgrid

--- client/src/components/pages/ContactUs.js
+++ client/src/components/pages/ContactUs.js
@@ -11,6 +11,35 @@ import NavBar from "../modules/NavBar.js";
 class ContactUs extends Component {
     constructor(props) {
       super(props);
+      this.state = {name: '', email: '', inquiry: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+      this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit(e) {
+      fetch('https://us-central1-the-artcubator.cloudfunctions.net/email', {
+        mode: 'cors',
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          message: `${this.state.name} (${this.state.email}) says: \n\n ${this.state.inquiry}`
+        })
+      }).then(data => {
+        data.json().then(jsondata => {
+          if (!jsondata.meow) {
+            alert('Something went wrong. Try again or email theartcubator@gmail.com. ')
+          } else {
+            alert('Thank you! A member of our team will get back to you soon. ')
+          }
+        })
+      })
     }
   
     render() {
@@ -22,19 +51,19 @@ class ContactUs extends Component {
                     <h1>purchase inquiries</h1>
                     <span>customer name</span>
                     <div>
-                        <input type="text" id="name" name="name" placeholder="Your name..."></input>
+                        <input onChange={this.handleChange} type="text" id="name" name="name" placeholder="Your name..."></input>
                     </div>
                     <br/>
                     <span>email</span>
                     <div>
-                        <input type="text" id="email" name="email" placeholder="Your email..."></input>
+                        <input onChange={this.handleChange} type="text" id="email" name="email" placeholder="Your email..."></input>
                     </div>
                     <br/>
                     <span>inquiry</span>
                     <div>
-                        <textarea type="text" id="inquiry" name="inquiry" placeholder="Your inquiry..."></textarea>
+                        <textarea onChange={this.handleChange} type="text" id="inquiry" name="inquiry" placeholder="Your inquiry..."></textarea>
                     </div>
-                    <button> submit </button>
+                    <button onClick={this.handleSubmit}> submit </button>
                 </div>
 
             </div>
